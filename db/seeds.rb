b390aeb36dc0d7b85c68f4d1d84ab821e41f1e41
Michael Raimondi <zeno@pobox.com>
2013-06-08 18:59:17 -0400

    Database seed creates assignments and challenges

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,7 @@ user = FactoryGirl.create(:user)
 
 30.times do 
     lesson = FactoryGirl.create(:lesson)
+    FactoryGirl.create(:activity, lesson: lesson)
+    FactoryGirl.create(:activity, lesson: lesson, position: 2)
+    activity = FactoryGirl.create(:activity, lesson: lesson, position: 3, completable: FactoryGirl.create(:challenge))
 end
