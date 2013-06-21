945d181f12d1e3fc6e7f436705cb155453cd4acc
Michael Raimondi <zeno@pobox.com>
2013-06-21 12:12:27 -0400

    Fix insecure secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,5 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Memworks::Application.config.secret_token = '58a059c0fc174f27cbc54cf4b78c05bebb489fef79d2838c48b6636bce0048cc9451c1004e63cd7e72cad72f57d464c390ed56e682b62b305f093322b3848e9a'
+Memworks::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
+  ('x' * 30) # meets minimum requirement of 30 chars long
+else
+  ENV['SECRET_TOKEN']
+end
