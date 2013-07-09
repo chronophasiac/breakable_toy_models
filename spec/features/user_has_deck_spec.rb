e53285fc2a4cdbcaca21cf36c7ec29f62ab8816c
Michael Raimondi <zeno@pobox.com>
2013-07-09 14:53:58 -0400

    Change 'Train' button 'Practice' and fix home page
    Rename 'Train' to 'Practice' on the top nav and main page. Correct
    typo on home page.

--- spec/features/user_has_deck_spec.rb
+++ spec/features/user_has_deck_spec.rb
@@ -16,36 +16,36 @@ feature "User has a deck of cards", %Q{
     visit(root_path)
   end
 
-  scenario "User sees a 'Train Now' button on the main page" do
-    expect(page).to have_link('Train Now')
+  scenario "User sees a 'Practice Now' button on the main page" do
+    expect(page).to have_link('Practice Now')
   end
 
-  scenario "User sees a 'Train' button on the top nav" do
+  scenario "User sees a 'Practice' button on the top nav" do
     within('.top-bar') do
-      expect(page).to have_link('Train')
+      expect(page).to have_link('Practice')
     end
   end
 
   scenario "User sees a card they've experienced before", js: true do
-    click_link('Train Now')
+    click_link('Practice Now')
     expect(page).to have_content(card.title)
   end
 
   scenario "User does not see a score", js: true do
-    click_link('Train Now')
+    click_link('Practice Now')
     expect(page).to_not have_css('.score-container')
   end
 
   scenario %Q{User sees a message that they haven't
     completed any cards if they have no cards}, js: true do
     CardSubmission.destroy_all
-    click_link('Train Now')
+    click_link('Practice Now')
     expect(page).to have_content("Sorry, you haven't completed any cards yet")
   end
 
   scenario "User sees a link to lessons", js: true do
     CardSubmission.destroy_all
-    click_link('Train Now')
+    click_link('Practice Now')
     within('.card-container') do
       click_link('Explore Lessons')
     end
